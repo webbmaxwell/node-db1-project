21231c8cf45d4337525308e7a06854faa54ad22d
webbmaxwell <webbwrites@gmail.com>
2020-10-01 15:05:52 -0600

    Added API methods for get, getById and post

--- api/server.js
+++ api/server.js
@@ -6,4 +6,40 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/budget', (req, res) => {
+    db('budget')
+    .then(budget => {
+        res.json(budget);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get budgets', err })
+    })
+});
+
+server.get('api/budget/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('budget').where({ id })
+    .then(budget => {
+        if (budget.length) {
+            res.json(budget)
+        } else {
+            res.status(404).json({ message: 'Could not find budget with given ID'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get budget', err })
+    })
+});
+
+server.post('/api/budget', (req, res) => {
+    db('budget').insert({ name: 'account-14', budget: '857' })
+    .then(budget => {
+        res.status(201).json({ message: 'Budget added'})
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Unable to add budget', err})
+    })
+})
+
 module.exports = server;
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -5,12 +5,12 @@ const knexfile = require("../knexfile.js");
 // change to "production" and update knexfile.js to use postgres.
 const database = "development";
 
-const config = {
-    client: 'sqlite3',
-    connection: {
-        filename: './data/budget.db3'
-    },
-    useNullAsDefault: true,
-}
+// const config = {
+//     client: 'sqlite3',
+//     connection: {
+//         filename: './data/budget.db3'
+//     },
+//     useNullAsDefault: true,
+// }
 
 module.exports = knex(knexfile[database]);
